dc18470fc5128f8a81803af973ca4a8ce101c7ea
Tyler Masthay <tyler@ices.utexas.edu>
2022-10-16 16:16:33 -0500

    Add files via upload

---
+++ get_started.sh
@@ -0,0 +1,2 @@
+python cross.py ext=txt input_files='["Final/Meals/Sweets/List_of_coffee_dishes/results.txt","Final/Meals/Sweets/List_of_desserts/results.txt"]' out_file='Queries/coffee_dessert' empty=False cartesian=True update=True
+python cross.py out_file='Queries/Chinese_Italian_soups' input_files='["Final/Meals/stews/List_of_Chinese_soups/results.txt","Final/Meals/stews/List_of_Italian_soups/results.txt"]' 
