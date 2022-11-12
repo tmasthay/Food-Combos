5bd856afc5849d61fd641d02f66100c412bfdb72
tmasthay <tyler@ices.utexas.edu>
2022-11-11 20:25:58 -0600

    Update database

--- gui.py
+++ gui.py
@@ -1,20 +1,35 @@
 from tkinter import *
+from tkinter import ttk
 from subprocess import check_output as co
 
 def extract(cmd):
     return co(cmd, shell=True).decode('utf-8').split('\n')[:-1]
 
+def button_call_back():
+    print('You clicked the button')
+
 files = extract('find Final -name "results.txt"')
 
-categories = [e.split('/')[1:] for e in files]
-
+paths = [e.split("/")[:-1] for e in files]
+indent_no = 4
+indent_char = '-'
+indented_files = []
+for p in paths:
+    for (i, pp) in enumerate(p):
+        curr = i*indent_no*indent_char + pp
+        if( i == len(pp) - 1 or (curr not in indented_files) ):
+            indented_files.append(curr)
+        
 top = Tk()
 
-Lb1 = Listbox(top)
-for (i,f) in enumerate(categories):
-    Lb1.insert(i+1, f)
+combobox = ttk.Combobox(top, values=indented_files, state='readonly')
+combobox.pack(fill='x')
 
-Lb1.pack()
+combobox = ttk.Combobox(top, values=indented_files, state='readonly')
+combobox.pack(fill='x')
+
+btn = Button(top, text='Run', command=button_call_back)
+btn.pack()
 
 top.geometry('1024x1024')
 top.mainloop()
